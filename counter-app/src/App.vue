412ed7a65393c9887df233a281f396cea12a2bbe
Seth Lewis <slandonlewis@gmail.com>
2024-02-22 11:44:56 -0600

    built basic counter app using ref and click events

---
+++ counter-app/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import {ref} from "vue"
+
+  const count = ref(0)
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>Count:</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count++">+</button>
+      <button @click="count--">-</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: aliceblue;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: black;
+  }
+
+  div {
+    font-size: 50px;
+    text-align: center;
+  }
+
+  button {
+    padding: 20px;
+    margin: 10px;
+    font-size: larger;
+  }
+</style>
