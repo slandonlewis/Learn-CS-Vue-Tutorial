caaeb356019874ca75d09e8d3819e9ee061cfe21
Seth Lewis <slandonlewis@gmail.com>
2024-02-26 13:12:06 -0600

    used defineProps to pass props between components

---
+++ quiz-app/src/components/Card.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { defineProps } from "vue"
+
+  const {quiz} = defineProps(['quiz']) // defined the quiz prop, which is each quiz that is rendered on the main App component
+</script>
+
+<template>
+<!-- Quiz object data passed in as a prop to the div -->
+    <div class="card">
+        <img :src="quiz.img">
+        <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>{{ quiz.questions.length}} questions</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card h2 {
+  font-weight: bold;
+}
+</style>
