b2ed068c54f5a3d6ac7a78c8a7fcf6dd38dd40a5
Seth Lewis <slandonlewis@gmail.com>
2024-02-26 15:19:12 -0600

    started dynamic routing based on what car is clicked

--- RouterVueLearning/src/views/HomeView.vue
+++ RouterVueLearning/src/views/HomeView.vue
@@ -1,5 +1,32 @@
+<script setup>
+    import cars from "../data/cars.json"
+    import {RouterLink} from "vue-router"
+</script>
+
 <template>
-    <div>
-        <h1>Home View</h1>
+    <h1>Home View</h1>
+    <div class="cars">
+        <RouterLink :to="`/cars/${car.id}`" v-for="car in cars" :key="car.id">
+            {{car.name}}
+        </RouterLink>
     </div>
 </template>
+
+<style scoped>
+    .cars {
+        display: flex;
+    }
+
+    .cars a {
+        text-decoration: none;
+        color: black;
+        width: 100%;
+        box-shadow: 1px 1px 1px black;
+        margin: 10px;
+    }
+
+    div {
+        background-color: aliceblue;
+        color: black;
+    }
+</style>
