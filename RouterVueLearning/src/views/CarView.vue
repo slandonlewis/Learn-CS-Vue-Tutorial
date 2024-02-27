054c11d06ee36213a612a7d7fbfb06b930922b00
Seth Lewis <slandonlewis@gmail.com>
2024-02-27 15:32:25 -0600

    display car details based on id

--- RouterVueLearning/src/views/CarView.vue
+++ RouterVueLearning/src/views/CarView.vue
@@ -1,5 +1,13 @@
+<script setup>
+    import { useRoute } from "vue-router"
+    import cars from "../data/cars.json"
+    const route = useRoute()
+    const car = cars.find(c => c.id === parseInt(route.params.id)) // return the car that matches the id in the url
+</script>
+
 <template>
     <div>
         <h1>Car View</h1>
+        {{ car }}
     </div>
 </template>
