47ec9a61df36795e73ab90dc710e47e4ba83258c
Seth Lewis <slandonlewis@gmail.com>
2024-02-27 16:01:03 -0600

    conditional rendering for car contact info

--- RouterVueLearning/src/views/CarView.vue
+++ RouterVueLearning/src/views/CarView.vue
@@ -1,9 +1,17 @@
 <script setup>
-    import { useRoute, RouterView } from "vue-router"
+    import { useRoute, RouterView, useRouter } from "vue-router"
     import cars from "../data/cars.json"
     const route = useRoute()
-    
-    const car = cars.find(c => c.id === parseInt(route.params.id)) // return the car that matches the id in the url
+    const router = useRouter()
+    const carId = parseInt(route.params.id)
+    const car = cars.find(c => c.id === carId) // return the car that matches the id in the url
+    const showContact = () => { // conditionally render contact info based on car id
+        if(carId === 3) {
+            return
+        } else {
+            return router.push(`/cars/${carId}/contact`)
+        }
+    }
 </script>
 
 <template>
@@ -12,6 +20,7 @@
         <p>{{car.name}}</p>
         <p>{{car.year}}</p>
         <p>{{car.price}}</p>
+        <button @click="showContact">Click for contact</button>
         <RouterView />
     </div>
 </template>
