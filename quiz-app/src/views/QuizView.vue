532e8575f3039e4e3ae6852165521b301a260206
Seth Lewis <slandonlewis@gmail.com>
2024-02-29 15:08:46 -0600

    finished app

--- quiz-app/src/views/QuizView.vue
+++ quiz-app/src/views/QuizView.vue
@@ -4,15 +4,20 @@
     import { useRoute } from 'vue-router'
     import { ref, watch, computed } from "vue"
     import quizes from "../data/quizes.json"
+    import Result from "../components/Result.vue"
 
     const route = useRoute()
     const quizId = parseInt(route.params.id)
     const currentQuestionIndex = ref(0)
     const numberOfCorrectAnswers = ref(0)
     const quiz = quizes.find(q => q.id === quizId)
+    const showResults = ref(false)
     const onOptionSelected = (isCorrect) => {
         if(isCorrect) {
             numberOfCorrectAnswers.value++
+        }
+        if (quiz.questions.length -1 === currentQuestionIndex.value) {
+            showResults.value = true
         }
         currentQuestionIndex.value++
     }
@@ -33,9 +38,9 @@
     <div>
         <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage "/>
         <div>
-            <Question :question="quiz.questions[currentQuestionIndex]" 
+            <Question v-if="!showResults" :question="quiz.questions[currentQuestionIndex]" 
             @selectOption="onOptionSelected"/>
+            <Result v-else :quizQuestionLength="quiz.questions.length" :numberOfCorrectAnswers="numberOfCorrectAnswers"/>
         </div>
-        <button @click="currentQuestionIndex++">Next Question</button>
     </div>
 </template>
