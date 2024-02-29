b4304f2e463af145c49372f173ea62acb027273f
Seth Lewis <slandonlewis@gmail.com>
2024-02-29 13:38:56 -0600

    implemented computed method for question status

--- quiz-app/src/views/QuizView.vue
+++ quiz-app/src/views/QuizView.vue
@@ -2,17 +2,20 @@
     import Question from "../components/Question.vue"
     import QuizHeader from "../components/QuizHeader.vue"
     import { useRoute } from 'vue-router'
-    import { ref, watch } from "vue"
+    import { ref, watch, computed } from "vue"
     import quizes from "../data/quizes.json"
 
     const route = useRoute()
     const quizId = parseInt(route.params.id)
     const currentQuestionIndex = ref(0)
     const quiz = quizes.find(q => q.id === quizId)
-    const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+    // const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
 
-    watch(() => currentQuestionIndex.value, () => {
-        questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+    // watch(() => currentQuestionIndex.value, () => {
+    //     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+    // })
+    const questionStatus = computed(() => {
+        return `${currentQuestionIndex.value}/${quiz.questions.length}`
     })
 </script>
 
